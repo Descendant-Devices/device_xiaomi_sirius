8d9856f866fc090ec0a4298f21d05fa1d1486436
spacetree1 <klepikov8561@gmail.com>
2020-03-22 12:00:47 +0300

    Descendant [1/2]

--- device.mk
+++ device.mk
@@ -68,10 +68,6 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.xiaomi_sdm710
-
 # PocketMode
 PRODUCT_PACKAGES += \
     XiaomiPocketMode
